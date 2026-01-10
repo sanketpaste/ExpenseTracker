7d55478e2968b202145fe2caa2d89970bcb0d7ad
sanketpaste <pastesanket28@gmail.com>
2026-01-10 20:20:03 +0530

    rename tab

--- App.js
+++ App.js
@@ -39,7 +39,7 @@ function ExpensesOverview() {
         name="RecentExpenses"
         component={RecentExpenses}
         options={{
-          title: 'Recent',
+          title: 'Recent Expenses',
           tabBarLabel: 'Recent',
           tabBarIcon: ({color, size}) => (
             <Fontawesome5 name="hourglass" color={color} size={size} />
